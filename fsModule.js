7c2d262a8d4188a458090c026298c63bc01a9d39
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-20 20:09:22 +0600

    fs module file delete and rename

--- fsModule.js
+++ fsModule.js
@@ -1,12 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-const myFilePath = path.join(__dirname, './sample.txt')
+const myFilePath = path.join(__dirname, 'files', './sample.txt')
+const myNewFilePath = path.join(__dirname, 'files', './sample2.txt')
+const myDirPath = path.join(__dirname, 'files')
 
 if (fs.existsSync(myFilePath)) {
-    fs.appendFileSync(myFilePath, 'This is next text')
+    // Append new text on current file
+    fs.appendFileSync(myFilePath, 'This is next text.\n')
     console.log('Data is written in the file');
 } else {
+    // Create file
     fs.writeFileSync(myFilePath, 'Welcome to Sample FS.\n');
 }
 
+// Delete file
+if(fs.existsSync(myFilePath)){
+    fs.unlink(myFilePath, () => {
+        console.log('File is deleted successfully')
+    })
+} else {
+    console.log('File is already removed')
+}
+
+// Rename file
+fs.rename(myFilePath, myNewFilePath, () => {
+    console.log('File is renamed')
+})
