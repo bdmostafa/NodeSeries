27b0c4c0c008f12acb46cc771af1cdfea4800e77
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-20 23:45:34 +0600

    http module, create server

--- http.js
+++ http.js
@@ -1,29 +1,42 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const Server = http.createServer((req, res) => {
     console.log(req.url)
-
+    const stream = fs.createReadStream(path.join(__dirname, 'index.html'))
     if(req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/html'})    
-        res.end(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            Hello world
-        </body>
-        </html>
-        `);
+        // res.end(`
+        // <!DOCTYPE html>
+        // <html lang="en">
+        // <head>
+        //     <meta charset="UTF-8">
+        //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        //     <title>Document</title>
+        // </head>
+        // <body>
+        //     Hello world
+        // </body>
+        // </html>
+        // `);
+        stream.pipe(res)
     }
 
     else if (req.url === '/hello') {
         res.writeHead(200, { 'Content-Type': 'text/html'})    
         res.end(`<p>Hello</p>`);
-    } else {
+    }else if (req.url === '/api/data') {
+        const users = [
+            {
+                id: 1,
+                name: 'Mostafa'
+            }
+        ]
+        res.writeHead(200, { 'Content-Type': 'text/json'})    
+        res.end(JSON.stringify(users));
+    } 
+     else {
         res.writeHead(200, { 'Content-Type': 'text/html'})    
         res.end('Not Found');
     }
