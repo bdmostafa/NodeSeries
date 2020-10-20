0203349a89a1949460bd7b0ab9e2e3fc3127bc3c
bdmostafa <mdmostafaalmahmud@gmail.com>
2020-10-20 20:25:52 +0600

    directory created and file append

--- fsModule.js
+++ fsModule.js
@@ -3,7 +3,9 @@ const path = require('path');
 
 const myFilePath = path.join(__dirname, 'files', './sample.txt')
 const myNewFilePath = path.join(__dirname, 'files', './sample2.txt')
-const myDirPath = path.join(__dirname, 'files')
+
+const myDirPath = path.join(__dirname, 'folder')
+const myFolderPath = path.join(__dirname, 'folder', './sample.txt')
 
 if (fs.existsSync(myFilePath)) {
     // Append new text on current file
@@ -15,7 +17,7 @@ if (fs.existsSync(myFilePath)) {
 }
 
 // Delete file
-if(fs.existsSync(myFilePath)){
+if (fs.existsSync(myFilePath)) {
     fs.unlink(myFilePath, () => {
         console.log('File is deleted successfully')
     })
@@ -27,3 +29,21 @@ if(fs.existsSync(myFilePath)){
 fs.rename(myFilePath, myNewFilePath, () => {
     console.log('File is renamed')
 })
+
+
+// Create folder/directory
+if (fs.existsSync(myDirPath)) {
+    console.log('Directory is already exist')
+} else {
+    fs.mkdir(myDirPath, {}, () => {
+        if (fs.existsSync(myFolderPath)) {
+            // Append new text on current file
+            fs.appendFileSync(myFolderPath, 'This is next text.\n')
+            console.log('Data is written in the file');
+        } else {
+            // Create file
+            fs.writeFileSync(myFolderPath, 'Welcome to the file in folder.\n');
+        }
+        console.log('Directory is created')
+    })
+}
